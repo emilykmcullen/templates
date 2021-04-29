c7a8a6726eed06ef673dae62251c812351712cc9
emilykmcullen <emilycullen135@gmail.com>
2021-04-29 18:03:54 +0100

    Initial Commit

---
+++ templates/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  templates
+//
+//  Created by Emily Cullen on 29/04/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
